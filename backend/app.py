7b76a6603512f768ad73b35e792e3b7c8f0ec1fd
Thrila <Ifeanyindubizu503@gmail.com>
2025-06-29 13:11:20 +0100

    feat: add url addr to cors

--- backend/app.py
+++ backend/app.py
@@ -11,11 +11,10 @@ from logic.main import (
 
 app = FastAPI()
 
+origins = ["http://localhost:5173", "http://192.168.131:5173", "*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
